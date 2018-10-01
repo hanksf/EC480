86a9c963004669730a79ada226daab79bf573726
hanksf <fergaldeclanhanks@gmail.com>
2018-10-01 18:10:35 -0500

    Did linear regression. Need to extract coefficients

--- hm1.R
+++ hm1.R
@@ -2,4 +2,15 @@
 library(foreign)
 mydata <- read.dta("C:/Users/Fergal/Documents/Northwestern/EC_480/Manski/INPUTM12.dta")
 
+mydata$a <- (mydata$f +mydata$m)/2
 
+fit <- lm(y ~ a, data=mydata)
+summary(fit)
+
+alpha <- 2:1; coefficients(lm(y ~ a, data=mydata))
+beta <- 2:2; coefficients(lm(y ~ a, data=mydata))
+
+fitted(fit)
+
+
+dens
